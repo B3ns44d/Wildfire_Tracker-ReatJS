cec2f967c89d40e210aa50f2a9c219519bfbbd7b
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-11-26 19:13:08 +0100

    start backend

---
+++ src/app/Loader.js
@@ -0,0 +1,12 @@
+import spinner from './logo.gif'
+
+export const Loader = () => {
+    return (
+        <div className="loader">
+            <img src={spinner} alt="loader"/>
+            
+        </div>
+    )
+}
+
+export default Loader
